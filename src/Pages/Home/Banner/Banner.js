9001b3a0ffb50550402d4adaea104924ac8f336c
Pranta07 <palpranta@gmail.com>
2021-11-15 16:24:11 +0600

    homepage banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,89 @@
 import React from "react";
-
+import Carousel from "react-material-ui-carousel";
+import { Paper, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 const Banner = () => {
+    var items = [
+        {
+            id: 1,
+            title: "The Classics",
+            subtitle: "Timeless & Elegant",
+            description:
+                "Complete your everyday look with a classic leather strap watch!",
+            bgImg: "https://i.ibb.co/Ry2s177/vitae-london-8-BOZ2-Tx-O2l-Q-unsplash.jpg",
+        },
+        {
+            id: 2,
+            title: "Best Seller",
+            subtitle: "Timeless & Elegant",
+            description:
+                "Complete your everyday look with a classic leather strap watch!",
+            bgImg: "https://i.ibb.co/5LsWnNz/vitae-london-pm-XO831uv-PU-unsplash.jpg",
+        },
+        {
+            id: 3,
+            title: "New Arrivals",
+            subtitle: "Timeless & Elegant",
+            description:
+                "Complete your everyday look with a classic leather strap watch!",
+            bgImg: "https://i.ibb.co/FB21gnX/bradley-ziffer-e-AOl9-QREIY0-unsplash.jpg",
+        },
+    ];
+    const style = {};
     return (
-        <div>
-            <h1>This is Banner</h1>
-        </div>
+        <Carousel>
+            {items.map((item) => (
+                <Paper
+                    style={{
+                        backgroundImage: `url(${item.bgImg})`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundPosition: "center center",
+                        backgroundSize: "cover",
+                        backgroundAttachment: "fixed",
+                        flexGrow: 1,
+                        height: "100vh",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Typography component="div">
+                        <Box
+                            sx={{
+                                letterSpacing: 2,
+                                m: 1,
+                                fontSize: "h6.fontSize",
+                                color: "white",
+                            }}
+                        >
+                            {item.subtitle}
+                        </Box>
+                        <Box
+                            sx={{
+                                fontWeight: "bold",
+                                fontFamily: "Monospace",
+                                m: 1,
+                                fontSize: "h2.fontSize",
+                                color: "white",
+                            }}
+                        >
+                            {item.title}
+                        </Box>
+                        <Box
+                            sx={{
+                                letterSpacing: 2,
+                                m: 1,
+                                fontSize: "h5.fontSize",
+                                color: "white",
+                            }}
+                        >
+                            {item.description}
+                        </Box>
+                    </Typography>
+                </Paper>
+            ))}
+        </Carousel>
     );
 };
 
--- src/Pages/PrivateRoute/PrivateRoute.js
+++ src/Pages/PrivateRoute/PrivateRoute.js
@@ -1,9 +1,14 @@
+import { CircularProgress } from "@mui/material";
 import React from "react";
 import { Redirect, Route } from "react-router";
 import useAuth from "../../hooks/useAuth";
 
 const PrivateRoute = ({ children, ...rest }) => {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
+    if (loading) {
+        <CircularProgress />;
+        return;
+    }
 
     return (
         <Route
