55d6838351fc50a382f9126ed864becd78bd62ff
Pranta07 <palpranta@gmail.com>
2022-02-15 00:57:10 +0600

    not found page ui updated

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,14 +1,78 @@
+import { ArrowForwardTwoTone } from "@mui/icons-material";
+import { Box, Button, Divider, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
     return (
-        <div>
-            <img
-                src="https://i.ibb.co/S548sPv/404-Error-Page-not-Found-with-people-connecting-a-plug-pana.png"
-                alt=""
-                width="100%"
-            />
-        </div>
+        <Box
+            sx={{
+                height: "100vh",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Box sx={{ m: 5 }}>
+                <Typography
+                    variant="h1"
+                    component="div"
+                    gutterBottom
+                    sx={{
+                        fontWeight: "bold",
+                        fontFamily: "Monospace",
+                    }}
+                >
+                    404
+                </Typography>
+                <Typography
+                    variant="h3"
+                    component="div"
+                    gutterBottom
+                    sx={{
+                        fontWeight: "bold",
+                        fontFamily: "Monospace",
+                    }}
+                >
+                    Oops! That page can't be found.
+                </Typography>
+                <Divider
+                    sx={{
+                        width: 200,
+                        p: 1,
+                        backgroundColor: "black",
+                        marginX: "auto",
+                        marginY: 5,
+                        "&:hover": {
+                            backgroundColor: "white",
+                            color: "black",
+                        },
+                    }}
+                ></Divider>
+
+                <Box sx={{ typography: "body2", fontFamily: "Monospace" }}>
+                    We're really sorry but we can't seem to find the page you
+                    were looking for.
+                </Box>
+
+                <Link to="/" style={{ textDecoration: "none" }}>
+                    <Button
+                        variant="contained"
+                        endIcon={<ArrowForwardTwoTone />}
+                        sx={{
+                            my: 5,
+                            px: 5,
+                            py: 1,
+                            backgroundColor: "black",
+                            color: "white",
+                            fontFamily: "Monospace",
+                        }}
+                    >
+                        BACK TO HOMEPAGE
+                    </Button>
+                </Link>
+            </Box>
+        </Box>
     );
 };
 
