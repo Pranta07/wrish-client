b423eadeda96ebf196eeb820c6cedf9e951c353f
Pranta07 <palpranta@gmail.com>
2022-02-15 14:21:30 +0600

    code refactored

--- src/Pages/Dashboard/MyOrders/MyOrders.js
+++ src/Pages/Dashboard/MyOrders/MyOrders.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -7,14 +9,12 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Box } from "@mui/system";
-import useAuth from "../../../hooks/useAuth";
 import {
     Delete,
     LocalShippingRounded,
     Payment,
     Pending,
 } from "@mui/icons-material";
-import Swal from "sweetalert2";
 import {
     Avatar,
     Grid,
@@ -24,41 +24,7 @@ import {
     Tooltip,
     Typography,
 } from "@mui/material";
-import { Link } from "react-router-dom";
-// import { DataGrid } from "@material-ui/data-grid";
-
-/* const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "firstName", headerName: "First name", width: 130 },
-    { field: "lastName", headerName: "Last name", width: 130 },
-    {
-        field: "age",
-        headerName: "Age",
-        type: "number",
-        width: 90,
-    },
-    {
-        field: "fullName",
-        headerName: "Full name",
-        description: "This column has a value getter and is not sortable.",
-        sortable: false,
-        width: 160,
-        valueGetter: (params) =>
-            `${params.row.firstName || ""} ${params.row.lastName || ""}`,
-    },
-];
-
-const rows = [
-    { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-    { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-    { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-    { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-    { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-    { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-    { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-    { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-    { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
-]; */
+import useAuth from "../../../hooks/useAuth";
 
 const MyOrders = () => {
     const { user } = useAuth();
@@ -74,13 +40,11 @@ const MyOrders = () => {
     };
 
     const handleChangeRowsPerPage = (event) => {
-        // console.log(event.target.value);
         setRowsPerPage(parseInt(event.target.value));
         setPage(0);
     };
 
     const handleDelete = (id) => {
-        // console.log(id);
         steIsDelete(false);
         Swal.fire({
             title: "Are you sure?",
@@ -97,7 +61,6 @@ const MyOrders = () => {
                 })
                     .then((res) => res.json())
                     .then((result) => {
-                        // console.log(result);
                         if (result.deletedCount) {
                             steIsDelete(true);
                             Swal.fire(
@@ -118,7 +81,6 @@ const MyOrders = () => {
         )
             .then((res) => res.json())
             .then((data) => {
-                // console.log(data);
                 setMyOrders(data.products);
                 setCount(data.count);
             })
@@ -265,15 +227,6 @@ const MyOrders = () => {
                     )}
                 </Box>
             </Grid>
-            {/*  <div style={{ height: 400, width: "100%" }}>
-                <DataGrid
-                    rows={rows}
-                    columns={columns}
-                    pageSize={5}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
-            </div> */}
         </>
     );
 };
